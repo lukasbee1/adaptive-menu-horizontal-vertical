97a7c5a8e25853e2f0b356d8d7a52db310576a7f
Lukashevich Maksim <lukashevich2014@gmail.com>
2019-03-30 22:14:07 +0300

    webpack init

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+let path = require('path');
+
+let ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+let conf = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+    publicPath: 'dist/'
+  },
+  devServer: {
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+  ]
+
+};
+
+module.exports = (env, options) =>{
+  let production = options.mode === 'production';
+  conf.devtool = production
+                              ? 'soursemap'
+                              : 'eval-soursemap';
+  return conf;
+}
